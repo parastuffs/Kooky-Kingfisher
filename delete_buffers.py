bb477aafaff97f0aefc8d65ed22d94c023e627e7
Quentin Delhaye <parastuffs@gmail.com>
2022-05-23 23:09:47 +0200

    Parse components and begining of nets.

--- delete_buffers.py
+++ delete_buffers.py
@@ -15,8 +15,92 @@ import datetime
 import os
 from docopt import docopt
 import logging, logging.config
-import sys
 from alive_progress import alive_bar
+import re
+
+
+def parseDEF(defFile):
+    """
+    Parse the DEF file to extract the components/nets relationships.
+
+    Parameters:
+    -----------
+    defFile:str
+        Path to DEF file.
+
+    Return:
+    -------
+    n/a
+    """
+    with open(defFile, 'r') as f:
+        lines = f.readlines()
+
+    expectedComponents = -1 # Number of components as stated on the 'COMPONENTS xxx' line
+    expectedNets = -1
+    instances = dict() # {instance name: stdCell name}
+    inNetDetails = False
+    nets = []
+
+    logger.info("Parsing DEF file...")
+    with alive_bar(len(lines)) as bar:
+        for line in lines:
+            bar()
+            if expectedComponents == -1:
+                # Typical line for components count look like:
+                # COMPONENTS 10875 
+                match = re.search('COMPONENTS (\d+)', line)
+                if match:
+                    expectedComponents = int(match.group(1))
+                    logger.info("Expecting {} components".format(expectedComponents))
+
+            # One we know the amount of components, it means we are entering the COMPONENTS part
+            else:
+                if expectedNets == -1:
+                    # Typical line for components looks like:
+                    # - DFFSR_692 DFFSR + PLACED ( 40 50 ) FS 
+                    match = re.search('- ([^\s]+) ([^\s]+) \+ PLACED', line)
+                    if match:
+                        instance = match.group(1)
+                        stdCell = match.group(2)
+                        instances[instance] = stdCell
+
+                    match = re.search('NETS (\d+)', line)
+                    if match:
+                        expectedNets = int(match.group(1))
+
+                # Once we know the amount of nets, we get into the NETS block.
+                # This is assuming that the nets are declared *after* the components.
+                else:
+                    # Once we reach the end of the nets description, quit the process.
+                    if "END NETS" in line:
+                        bar(len(lines)-bar.current()) # Jump the progress bar to the end
+                        break
+                    if not inNetDetails:
+                        # Try to fetch a net name
+                        match = re.search('- ([^\s\n]+)', line)
+                        if match:
+                            netName = match.group(1)
+                            nets.append(netName)
+                            inNetDetails = True
+                    elif inNetDetails:
+                        # We reached the end of the relevant part
+                        if "ROUTED" in line or ";" in line or 'PROPERTY' in line or 'SOURCE' in line:
+                            inNetDetails = False
+                        # We are still in the 'connectivity' part of the net details
+                        # else:
+
+
+
+
+
+
+        logger.info("Found {} components out of {} expected ({}%)".format(len(instances), expectedComponents, 100*(len(instances)/expectedComponents)))
+        logger.info("Found {} nets ou of {} expected ({}%)".format(len(nets), expectedNets, 100*(len(nets)/expectedNets)))
+
+
+
+
+
 
 if __name__ == "__main__":
 
@@ -55,3 +139,5 @@ if __name__ == "__main__":
     logger.addHandler(fh)
 
     logger.info("Let's go")
+
+    parseDEF(defFile)
